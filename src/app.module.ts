8e797cc652c1202e1a1c80608abd13f7e861af83
Kelvin Mai <kelvin.mai002@gmail.com>
2018-10-13 20:20:10 -0400

    Added graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { ApiModule } from './api.module';
@@ -6,7 +7,13 @@ import { AppController } from './app.controller';
 import { AppGateway } from './app.gateway';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), ApiModule],
+  imports: [
+    TypeOrmModule.forRoot(),
+    GraphQLModule.forRoot({
+      typePaths: ['./**/*.graphql'],
+    }),
+    ApiModule,
+  ],
   controllers: [AppController],
   providers: [AppGateway],
 })
