c828a25602309ede4097420fab49c8443d3775d1
Kelvin Mai <kelvin.mai002@gmail.com>
2018-10-22 19:15:54 -0400

    Enabled cors

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,13 @@ const port = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      'http://localhost:4200', // angular
+      'http://localhost:3000', // react
+      'http://localhost:8081', // react-native
+    ],
+  });
   await app.listen(4000);
   Logger.log(`Server running on http://localhost:${port}`, 'Bootstrap');
 }
